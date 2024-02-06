f0cb6cc296c58e8a2081517cc3a8b7c3df669cfe
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-02-07 00:16:02 +0530

    change nav link path  of tech page

--- Sanjay_Vishwakarma/tailwind.config.js
+++ Sanjay_Vishwakarma/tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./dist/global.html"],
+  mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+    },
   },
+  variants: {},
+ 
+  darkMode: 'class',
+  darkMode: ['class', '[data-mode="dark"]'],
   plugins: [],
 }
